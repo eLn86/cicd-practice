7936ef602b09c690955b8a7018bfbbd92cb4a786
eLn86 <elnathan.erh@gmail.com>
2018-03-28 10:31:31 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">CI/CD Practice</h1>
+        </header>
+        <p className="App-intro">
+          Single Page React App using CRA to test CICD pipeline.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
